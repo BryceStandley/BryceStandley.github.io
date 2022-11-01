b0cf881ecf169b217a4444cff8d69fed56cfd02b
Bryce Standley <bryce@vectorpixel.net>
2022-11-01 15:17:44 +0800

    Updated discord contact hook info

--- assets/js/discord.js
+++ assets/js/discord.js
@@ -3,17 +3,17 @@ async function SendContactMessage(event)
 {
     event.preventDefault();
 
-    const senderEmail = document.getElementById("email").value;
-    const senderName = document.getElementById("name").value;
-    const senderMsg = document.getElementById("message").value;
+    const senderEmail = document.getElementById("email");
+    const senderName = document.getElementById("name");
+    const senderMsg = document.getElementById("message");
 
-    if(senderName === '' || senderEmail === '' || senderMsg === '')
+    if(senderName.value === '' || senderEmail.value === '' || senderMsg.value === '')
     {
       alert('Please enter the required contact information.');
       return;
     }
 
-    const messageJson = {'name':senderName, 'email':senderEmail, 'message':senderMsg};
+    const messageJson = {'name':senderName.value, 'email':senderEmail.value, 'message':senderMsg.value};
     const webhookUrl = 'https://api.vectorpixel.net/validate/hooks/'
 
     const response = await fetch(webhookUrl, {
@@ -22,6 +22,9 @@ async function SendContactMessage(event)
       body: JSON.stringify(messageJson),
     }).then(res => {
       if (res.status === 200) {
+        senderName.value = '';
+        senderEmail.value = '';
+        senderMsg.value = '';
         alert('I have received your message!');
       } else {
         alert('There was an error! Try again later!');
