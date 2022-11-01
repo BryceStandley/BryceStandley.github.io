367d3ffdaa528e7f2590c7728c65e17b8e4fdd48
Bryce Standley <bryce@vectorpixel.net>
2022-11-01 13:51:07 +0800

    Update discord.js

--- assets/js/discord.js
+++ assets/js/discord.js
@@ -7,33 +7,22 @@ async function SendContactMessage(event)
     const senderName = document.getElementById("name").value;
     const senderMsg = document.getElementById("message").value;
 
-
-    const webhookBody = {
-        embeds: [{
-          title: 'Portfolio Contact Form Submitted',
-          fields: [
-            { name: 'Sender', value: senderName },
-            { name: 'Email', value: senderEmail },
-            { name: 'Message', value: senderMsg }
-          ]
-        }],
-      };
-
-      const webhookUrl = 'https://discord.com/api/webhooks/1034392675383779338/xYVhdwqAG0EudeSpA4Y00YpCCp6wjmSyPMiVyEuK3OvDYi3Wle2_gG8qH3ChCAwdWxI5';
+    const messageJson = {'name':senderName, 'email':senderEmail, 'message':senderMsg};
+    const webhookUrl = 'https://api.vectorpixel.net:5050/validate/hooks'
 
     const response = await fetch(webhookUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(webhookBody),
-    });
-
-    if (response.ok) {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(messageJson),
+    }).then(res => {
+      if (res.status === 200) {
         alert('I have received your message!');
       } else {
         alert('There was an error! Try again later!');
       }
+    });
+
+    
       
       
 }
